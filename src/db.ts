bd0eec9159d474b4dc3c4ba9e6728fc473db2485
Andrii Blyshchak <andrewblyshchak@gmail.com>
2021-01-12 21:01:28 +0200

    Added post/put/delete routes and functions

--- src/db.ts
+++ src/db.ts
@@ -12,3 +12,58 @@ export const getTeachers = async (req: Request, res: Response): Promise<Response
     return res.status(500).json({ msg: 'Internal Server Error(500)' });
   }
 };
+
+export const getTeacherById = async (req: Request, res: Response): Promise<Response> => {
+  const id = parseInt(req.params.id, 10);
+  const response: QueryResult = await pool.query('SELECT * FROM teachers WHERE id = $1', [id]);
+  return res.json(response.rows);
+};
+
+// export const getTeacherByParams = async (req: Request, res: Response): Promise<Response> => {
+
+// };
+
+export const createTeacher = async (req: Request, res: Response): Promise<Response> => {
+  const { firstName, lastName, age, experience } = req.body;
+  const response: QueryResult = await pool.query('INSERT INTO teachers (firstName, lastName, age, experience) VALUES ($1, $2, $3, $4)', [firstName, lastName, age, experience]);
+  return res.json({
+    teacher: {
+      firstName,
+      lastName,
+      age,
+      experience,
+    },
+  });
+};
+
+export const updateTeacher = async (req: Request, res: Response): Promise<Response> => {
+  const id = parseInt(req.params.id, 10);
+  const { firstName, lastName, age, experience } = req.body;
+  const response: QueryResult = await pool.query('UPDATE teachers SET firstName = $1, lastName = $2, age = $3, experience = $4 WHERE id = $5', [firstName, lastName, age, experience, id]);
+  return res.json({ msg: `Teacher ${id} has been updated successfully` });
+};
+
+export const deleteTeacher = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    const response: QueryResult = await pool.query('DELETE FROM teachers WHERE id = $1', [id]);
+    return res.json({ msg: `Teacher ${id} has been deleted successfully` });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: 'Internal Server Error(500)' });
+  }
+};
+
+export const deleteAllTeachers = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const response: QueryResult = await pool.query('DELETE FROM teachers');
+    return res.json({ msg: 'All teachers have been deleted successfully' });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json('Internal Server Error');
+  }
+};
+
+// export const getTargetMathTeachers = async (req: Request, res: Response): Promise<Response> => {
+
+// };
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
-import { getTeachers } from './db';
+import { getTeachers, getTeacherById, createTeacher,
+  updateTeacher, deleteTeacher, deleteAllTeachers } from './db';
 
 const router = Router();
 
 router.get('/teachers', getTeachers);
+router.get('/teachers/:id', getTeacherById);
+router.post('/teachers', createTeacher);
+router.put('/teachers/:id', updateTeacher);
+router.delete('/teachers/:id', deleteTeacher);
+router.delete('/teachers', deleteAllTeachers);
+// router.get('/teachers/:age:sex:experience', getTeacherByParams);
+// router.get('/teachers', getTargetMathTeachers);
 
 export default router;
